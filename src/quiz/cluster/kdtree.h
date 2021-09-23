b3284c867600fa1afad93bd8a733d48ae96d5355
Dobuzi <kimjw7981@gmail.com>
2021-09-23 07:35:36 +0000

    completed cluster quiz with search

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -56,19 +56,45 @@ struct KdTree
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
-		insertHelper(&root, point, id, false);
+		insertHelper(&root, point, id, false);  
 	}
 
+	void searchHelper(std::vector<float> target, float distanceTol, std::vector<int> *ids, Node **node, bool isY)
+	{
+		float max_val_x = target[0] + distanceTol;
+		float min_val_x = target[0] - distanceTol;
+		float max_val_y = target[1] + distanceTol;
+		float min_val_y = target[1] - distanceTol;
+		float val_x, val_y;
 
+		if (*node == NULL)
+		{
+			return;
+		}
+		else
+		{
+			val_x = (*node)->point[0];
+			val_y = (*node)->point[1];
+
+			// Check the node is located in the boundary of target
+			if ((min_val_x <= val_x) && (val_x <= max_val_x) && (min_val_y <= val_y) && (val_y <= max_val_y))
+			{
+				// Then add id to ids
+				ids->push_back ((*node)->id);
+			}
+
+			searchHelper(target, distanceTol, ids, &((*node)->left), isY);
+			searchHelper(target, distanceTol, ids, &((*node)->right), isY);
+		}
+	}
 
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		searchHelper(target, distanceTol, &ids, &root, false);
 		return ids;
 	}
-	
-
 };
 
 
