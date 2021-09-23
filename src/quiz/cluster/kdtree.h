9c948bba23e98b8a5640981ad4e1d833a7be2923
Dobuzi <kimjw7981@gmail.com>
2021-09-23 07:53:23 +0000

    completed cluster quiz (search)

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -59,32 +59,43 @@ struct KdTree
 		insertHelper(&root, point, id, false);  
 	}
 
-	void searchHelper(std::vector<float> target, float distanceTol, std::vector<int> *ids, Node *node)
+	void searchHelper(std::vector<float> target, float distanceTol, std::vector<int> *ids, Node *node, bool isY)
 	{
 		float max_val_x = target[0] + distanceTol;
 		float min_val_x = target[0] - distanceTol;
 		float max_val_y = target[1] + distanceTol;
 		float min_val_y = target[1] - distanceTol;
-		float val_x, val_y;
+		float pt_x, pt_y, dist_x, dist_y, distance;
+		bool in_range_x, in_range_y;
 
-		if (node == NULL)
+		if (node != NULL)
 		{
-			return;
-		}
-		else
-		{
-			val_x = node->point[0];
-			val_y = node->point[1];
-
+			pt_x = node->point[0];
+			pt_y = node->point[1];
+			in_range_x = (min_val_x <= pt_x) && (pt_x <= max_val_x);
+			in_range_y = (min_val_y <= pt_y) && (pt_y <= max_val_y);
 			// Check the node is located in the boundary of target
-			if ((min_val_x <= val_x) && (val_x <= max_val_x) && (min_val_y <= val_y) && (val_y <= max_val_y))
+			if (in_range_x && in_range_y)
 			{
-				// Then add id to ids
-				ids->push_back (node->id);
+				dist_x = target[0] - pt_x;
+				dist_y = target[1] - pt_y;
+				distance = sqrt(dist_x * dist_x + dist_y * dist_y);
+
+				if (distance < distanceTol)
+				{
+					ids->push_back (node->id); // Then add id to ids
+				}
+			}
+			
+			if (target[int(isY)]-distanceTol < node->point[int(isY)])
+			{
+				searchHelper(target, distanceTol, ids, node->left, !isY);
 			}
 
-			searchHelper(target, distanceTol, ids, node->left);
-			searchHelper(target, distanceTol, ids, node->right);
+			if (target[int(isY)]+distanceTol > node->point[int(isY)])
+			{
+				searchHelper(target, distanceTol, ids, node->right, !isY);
+			}
 		}
 	}
 
@@ -92,7 +103,7 @@ struct KdTree
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
-		searchHelper(target, distanceTol, &ids, root);
+		searchHelper(target, distanceTol, &ids, root, false);
 		return ids;
 	}
 };
