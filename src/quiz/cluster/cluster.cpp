28d462cdde8183f323da8ec97e05122846f5a400
Dobuzi <kimjw7981@gmail.com>
2021-09-26 13:16:19 +0000

    finish the euclidean clustering quiz with mentions

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -79,16 +79,16 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 
 void proximity(std::vector<std::vector<float>> points, int i, std::vector<int> *cluster, KdTree* tree, float distanceTol, std::unordered_set<int> *processed)
 {
-	processed->insert (i);
-	cluster->push_back (i);
+	processed->insert (i); // mark point as processed
+	cluster->push_back (i); // add point to cluster
 	
-	std::vector<int> nearby = tree->search(points[i], distanceTol);
+	std::vector<int> nearby = tree->search(points[i], distanceTol); // nearby points = tree(point)
 
-	for (int j = 0; j < nearby.size(); j++)
+	for (int j = 0; j < nearby.size(); j++) // Iterate through each nearby point
 	{
-		if (processed->find(nearby[j]) == processed->end())
+		if (processed->find(nearby[j]) == processed->end()) // If point has not been processed
 		{
-			proximity(points, nearby[j], cluster, tree, distanceTol, processed);
+			proximity(points, nearby[j], cluster, tree, distanceTol, processed); // Proximity(cluster)
 		}
 	}
 }
@@ -97,23 +97,17 @@ std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<flo
 {
 	// TODO: Fill out this function to return list of indices for each cluster
 
-	std::vector<std::vector<int>> clusters;
-	std::unordered_set<int> processed;
-	std::vector<int> cluster;
+	std::vector<std::vector<int>> clusters; // list of clusters
+	std::unordered_set<int> processed; // processing marker
+	std::vector<int> cluster; // Define cluster
 
-	for (int i = 0; i < points.size (); i++)
+	for (int i = 0; i < points.size (); i++) // Iterate through each point
 	{	
-		if (processed.find(i) == processed.end())
+		if (processed.find(i) == processed.end()) // If point has not been processed
 		{
-			cluster.clear();
-			proximity(points, i, &cluster, tree, distanceTol, &processed);
-			std::cout << "cluster " << clusters.size() << ": ";
-			for (int j = 0; j < cluster.size(); j++)
-			{
-				std::cout << cluster[j] << " ";
-			}
-			std::cout << std::endl;
-			clusters.push_back (cluster);
+			cluster.clear(); // Clear cluster (== Create cluster)
+			proximity(points, i, &cluster, tree, distanceTol, &processed); // Proximity(point, cluster)
+			clusters.push_back (cluster); // cluster add clusters
 		}
 	}
 	
