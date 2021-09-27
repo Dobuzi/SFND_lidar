0412b42623d792d210a2eda6692158b57715d1b7
Dobuzi <kimjw7981@gmail.com>
2021-09-27 01:33:49 +0000

    working with data_2

--- src/environment.cpp
+++ src/environment.cpp
@@ -236,7 +236,7 @@ int main (int argc, char** argv)
     else
     {
     	ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
-		std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_1");
+		std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_2");
 		auto streamIterator = stream.begin();
     	pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloudI; 
 
